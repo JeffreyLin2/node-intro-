d19900d2493c51931ef67d54ea269b72964dcd81
Jeffrey Lin <jeffrey.j.w.lin@gmail.com>
2020-11-17 22:02:41 -0300

    feature: criando servidor

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-const cumprimentar =require('./modules/cumprimentar')
-const nome = 'Jeffrey'
+const http = require('http')
 
-console.log (cumprimentar(nome))
+http.createServer((req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'application ld/json'
+  })
+  console.log('Servidor rodando corretamente!')
+  res.end('[{"nome":"Fulano","sobrenome":"Silva"},{"nome":"Ciclano","sobrenome":"Silva"},{"nome":"Beltrano","sobrenome":"Silva"}]')
+}).listen(3030, 'localhost')
